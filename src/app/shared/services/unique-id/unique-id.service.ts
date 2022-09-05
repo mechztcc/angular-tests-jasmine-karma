e27763f0db6ad99091572550269bb0ca3e377f1c
mechztcc <dojathan@gmail.com>
2022-09-05 18:51:14 -0400

    feat: created service to generate unique id

---
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UniqueIdService {
+  private numberOfGeneratedIds = -0;
+
+  constructor() {}
+
+  generateUniqueIdWIthPrefix(prefix: string): string {
+    if (!prefix) {
+      throw Error('Prefix can not be empty');
+    }
+    const uniqueId = this.generaUniqueId();
+    this.numberOfGeneratedIds++;
+    return `${prefix}-${uniqueId}`;
+  }
+
+  getNumberOfGeneratedUniqueIds(): number {
+    return this.numberOfGeneratedIds;
+  }
+
+  private generaUniqueId(): string {
+    return uuid();
+  }
+}
