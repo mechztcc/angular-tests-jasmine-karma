44f3cf6e5557827c824c34b3fcc5f9725d13ea43
mechztcc <dojathan@gmail.com>
2022-09-06 09:01:17 -0400

    feat: add beforeEach on tests suit

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,21 +1,30 @@
 import { UniqueIdService } from './unique-id.service';
 
 describe(UniqueIdService.name, () => {
+  let service: UniqueIdService;
+  beforeEach(() => {
+    service = new UniqueIdService();
+  });
+
   it(`#${UniqueIdService.prototype.generateUniqueIdWIthPrefix.name} should generate id when called with prefix`, () => {
-    const service = new UniqueIdService();
     const id = service.generateUniqueIdWIthPrefix('app');
 
     expect(id.startsWith('app-')).toBeTrue();
   });
 
   it(`#${UniqueIdService.prototype.generateUniqueIdWIthPrefix.name} should not generate duplicated ids when called with multiple times`, () => {
-    const service = new UniqueIdService();
-
     const ids = new Set();
     for (let index = 0; index < 50; index++) {
       ids.add(service.generateUniqueIdWIthPrefix('app'));
     }
 
     expect(ids.size).toBe(50);
+  });
+
+  it(`${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generated ids when called`, () => {
+    service.generateUniqueIdWIthPrefix('app');
+    service.generateUniqueIdWIthPrefix('app');
+
+    expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
   });
 });
