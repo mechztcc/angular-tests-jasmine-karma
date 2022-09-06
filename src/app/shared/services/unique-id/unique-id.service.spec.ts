df67c205f47b0a0ab6f156a4d4dc44bdf8cdf653
mechztcc <dojathan@gmail.com>
2022-09-05 20:06:37 -0400

    fix: add new test to unique id service

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -7,4 +7,15 @@ describe(UniqueIdService.name, () => {
 
     expect(id.startsWith('app-')).toBeTrue();
   });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWIthPrefix.name} should not generate duplicated ids when called with multiple times`, () => {
+    const service = new UniqueIdService();
+
+    const ids = new Set();
+    for (let index = 0; index < 50; index++) {
+      ids.add(service.generateUniqueIdWIthPrefix('app'));
+    }
+
+    expect(ids.size).toBe(50);
+  });
 });
