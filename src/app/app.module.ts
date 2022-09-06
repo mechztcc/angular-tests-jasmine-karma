43cd4b09fabd6b7e24f35b137b09139db2887e96
mechztcc <dojathan@gmail.com>
2022-09-06 10:26:23 -0400

    feat: created liked components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LikeWidgetComponent } from './shared/components/like-widget/like-widget.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, LikeWidgetComponent],
+	imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/components/like-widget/like-widget.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+	selector: 'app-like-widget',
+	templateUrl: './like-widget.component.html',
+	styleUrls: ['./like-widget.component.scss'],
+})
+export class LikeWidgetComponent implements OnInit {
+	@Output() public liked = new EventEmitter<void>();
+	@Input() public likes = 0;
+  @Input() public id;
+
+	constructor() {}
+
+	ngOnInit(): void {}
+}
